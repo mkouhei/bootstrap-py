021cf4048958fea2624f27a3634e18e97aa5c76c
Kouhei Maeda <mkouhei@palmtb.net>
2016-02-01 04:09:03 +0900

    Renames HISTORY to CHANGELOG.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ classifiers = [
 
 long_description = (
     read_content("README.rst") +
-    read_content(os.path.join("docs/source", "HISTORY.rst")))
+    read_content(os.path.join("docs/source", "CHANGELOG.rst")))
 
 requires = ['setuptools',
             'Jinja2',
