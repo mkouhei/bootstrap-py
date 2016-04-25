0a05b516382135d75f0e91fbd35ee5a6a7b1f51f
Kouhei Maeda <mkouhei@palmtb.net>
2016-04-25 13:55:23 +0900

    Unsupports IP address url.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- bootstrap_py/commands.py
+++ bootstrap_py/commands.py
@@ -60,9 +60,7 @@ def valid_url(url):
     regex = re.compile(
         r'^(?:http)s?://'
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
-        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
+        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?))'
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     if not regex.match(url):
         raise argparse.ArgumentTypeError('"{0}" is invalid url.'.format(url))
