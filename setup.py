6ee66406d3bc71c3c99f2d12aa07b76065aee878
Kouhei Maeda <mkouhei@palmtb.net>
2018-11-03 08:36:24 +0900

    Unsupport PyPy.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ classifiers = [
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: Implementation :: CPython",
-    "Programming Language :: Python :: Implementation :: PyPy",
     "Topic :: Software Development",
     "Topic :: System :: Archiving :: Packaging",
     "Topic :: System :: Software Distribution",
