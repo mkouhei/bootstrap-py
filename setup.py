af3cfc60a6db41ee7d037e0fe937a73d9ede3752
Kouhei Maeda <mkouhei@palmtb.net>
2020-04-26 21:20:00 +0900

    Updates copyright.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (C) 2016-2019 Kouhei Maeda <mkouhei@palmtb.net>
+Copyright (C) 2016-2020 Kouhei Maeda <mkouhei@palmtb.net>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
